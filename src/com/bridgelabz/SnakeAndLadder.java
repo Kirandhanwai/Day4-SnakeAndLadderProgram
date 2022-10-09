a3ceb63c9fbe03ce085c03d0a4750b69eca6bbf7
Kirandhanwai <kirandhanwai123@gmail.com>
2022-10-10 00:53:12 +0530

    Added: Random Dice Roll

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,19 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 	
 	static int position = 0;
+	
+	static int diceRoll() {
+		int diceRoll = (int)(Math.random()*10)%6 + 1;
+		return diceRoll;
+	}
+	
 	public static void main(String[] Args) {
-		
+	int diceRoll = diceRoll();	
+	
 	}
 
 }
